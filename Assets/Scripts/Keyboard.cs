c6bc308a810563d141f59c9d0a67c6f1782a16ac
AashritParvangada <aashrit.parvangada@gmail.com>
2019-04-16 07:36:45 +0530

    Added Keys to Game

---
+++ Assets/Scripts/Keyboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void GetAllKeys()
+	{
+		foreach(GameObject _key in GetComponentsInChildren<GameObject>())
+		{
+			//Add key to list.
+		}
+	}
+
+}
