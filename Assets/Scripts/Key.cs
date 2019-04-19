fbac62405e20e41b5bace2f95d489c0b6c7c84df
AashritParvangada <aashrit.parvangada@gmail.com>
2019-04-19 10:46:04 +0530

    Added Key Script

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+	private void OnMouseDown() {
+		Debug.Log("Gotcha");
+		Color dimmed = new Color(50,50,50);
+		GetComponent<SpriteRenderer>().color=dimmed;
+	}
+
+}
