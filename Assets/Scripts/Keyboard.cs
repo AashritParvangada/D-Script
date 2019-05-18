1cd6709f25d7f3daf270aee5fee9974a02c13018
AashritParvangada <aashrit.parvangada@gmail.com>
2019-05-18 08:48:55 +0530

    Inputting Keys works for the whole level. The prototype is nearly ready, great work!

--- Assets/Scripts/Keyboard.cs
+++ Assets/Scripts/Keyboard.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Keyboard : MonoBehaviour
 {
-    Transform t_oldKeyPosition;     //Used to set the key back to the original when the keyboard is reset (i.e. when the answer is correct or incorrect)
-
+    Vector3 t_oldKeyPosition;     //Used to set the key back to the original when the keyboard is reset (i.e. when the answer is correct or incorrect)
+    Vector3 t_oldKeyScale;
+    Key Key_SelectedKey;
     [SerializeField] Vector3 V3_CenterPosition = new Vector3(0, -4, 0); //Where the center position is. Can be edited.
     [SerializeField] float F_NewScale = 1; //What the center scale is. Can be edited.
 
@@ -60,6 +61,7 @@ public class Keyboard : MonoBehaviour
     {
         if (!b_NormalKeyClicked)
         {
+            Key_SelectedKey = _keyClicked;
             SwitchOffOthers(_keyClicked);
             CenterKey(_keyClicked);
             b_NormalKeyClicked = true;
@@ -116,9 +118,11 @@ public class Keyboard : MonoBehaviour
     //Bring the key to the center based on serialised variables.
     void CenterKey(Key _keyToCenter)
     {
-        t_oldKeyPosition = _keyToCenter.transform;
+        t_oldKeyPosition = _keyToCenter.transform.position;
+        t_oldKeyScale = _keyToCenter.transform.localScale;
         _keyToCenter.transform.position = V3_CenterPosition;
         _keyToCenter.transform.localScale = new Vector3(F_NewScale, F_NewScale, F_NewScale);
+
     }
 
     //This is still a WIP. It still needs to be tied in to the actual key that has been clicked.
@@ -144,16 +148,46 @@ public class Keyboard : MonoBehaviour
         if (s_inputLetter == LetFor_correctKey.S_BaseLetter)
         {
             Debug.Log("That was the correct input!");
+            SwitchOffTriggerOnKey();
+            ResetAndSwitchOffKeyboard();
             ScrWri_Level.InstantiateSingleDevanagari(LetFor_correctKey);
         }
 
         else
         {
             {
+                ReturnKey();
+                b_NormalKeyClicked=false;
+                b_DiacriticKeyClicked=false;
+                SwitchOnOffDiacritics(b_NormalKeyClicked);
+                SwitchOnRegulars();
+                s_inputLetter=null;
                 Debug.Log("Wrong input!" + LetFor_correctKey.S_BaseLetter + "!=" + s_inputLetter);
             }
         }
     }
 
+    void ResetAndSwitchOffKeyboard() //This is called when players get a key correctly. The Keyboard disappears.
+    {
+        ReturnKey();
+        b_NormalKeyClicked=false;
+        b_DiacriticKeyClicked=false;
+        SwitchOnOffDiacritics(false);
+        SwitchOffKeyboard();
+        s_inputLetter=null;
+
+    }
+
+    //Returns a key to the place and scale from whence it came.
+    void ReturnKey()
+    {
+        Key_SelectedKey.transform.position = t_oldKeyPosition;
+        Key_SelectedKey.transform.localScale = t_oldKeyScale;
+    }
+
+    void SwitchOffTriggerOnKey() //Currently makes sure we can't click the Hiragana again. Later can put a funciton in the hiragana that affects animation.
+    {
+        LetFor_correctKey.GetComponent<BoxCollider2D>().enabled=false;
+    }
 
 }
